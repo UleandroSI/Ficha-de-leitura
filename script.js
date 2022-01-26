36ea94721a77925dadb199bf2aebb6c535c4850c
Leandro Barbosa <uleandrosp7@gmail.com>
2022-01-26 05:03:07 -0300

    1.1.13

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function Salvar() {
     let formulario = []
-    let titulo = document.getElementById("inputTitulo").innerText
+    let res = document.getElementById("res")
+    let titulo = document.getElementById("inputTitulo").innerHTML
+/*    titulo = titulo.innerText*/
+    formulario.push(titulo)
     let autor = document.getElementById("inputAutor").innerText
+    formulario.push(autor)
     let publicacao =  document.getElementById("inputPublicacao").innerText
+    formulario.push(publicacao)
     let assunto = document.getElementById("inputAssunto").innerText
+    formulario.push(assunto)
     let recursos =  document.getElementById("inputRecursos").innerText
     let teses = document.getElementById("inputTeses").innerText
     let evidencias = document.getElementById("inputEvidencias").innerText
@@ -11,9 +17,14 @@ function Salvar() {
     let contribuicao =  document.getElementById("inputContribuicao").innerText
     let interpretacoes = document.getElementById("inputInterpretacoes").innerText
     let problemas = document.getElementById("inputProblemas").innerText
-
-    formulario.push(titulo, autor, publicacao, assunto, recursos, teses, evidencias, desconhecidos, contribuicao, interpretacoes, problemas)
-    limpar()
+    
+    /*console.log(`${formulario}`)*/
+    /*document.getElementById("res").innerHTML = `${titulo}`*/
+    console.log(`${titulo}`)
+    document.getElementById("res").innerHTML += `${autor}`
+    console.log(`${autor}`)
+    document.getElementById("res").innerHTML += `${publicacao}`
+    Limpar()
 }
 
 function Limpar() {
@@ -21,12 +32,12 @@ function Limpar() {
 }
 
 function Imprimir() {
-    
+    print()
 }
 
 function Visualizar() {
-    let titulo = document.getElementById("inputTitulo").innerText
     tela_impressao = window.open('about:blank')
     tela_impressao.document.write(titulo)
     tela_impressao.window.print()
+    res.innerHTML = `${formulario}`
 }
