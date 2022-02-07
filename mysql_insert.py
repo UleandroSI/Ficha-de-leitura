f92e0f7957fdbdcdd9ab4fe1a7fbeff07c423792
Leandro Barbosa <uleandrosp7@gmail.com>
2022-02-07 12:32:46 -0300

    1.1.22

--- mysql_insert.py
+++ mysql_insert.py
@@ -22,14 +22,6 @@ inserido = inserir(autor, publicacao, assunto, recursos, teses, evidencias, desc
 """ Criar tabela
 import mysql.connector
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="ti",
-  password="zt049br",
-  database="livros_db"
-)
-
-
 mycursor = mydb.cusor()
 
 
